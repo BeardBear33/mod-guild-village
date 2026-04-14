27cc9c16a5d5ab744483d86bcd00352a50c629aa
BeardBear <138500494+BeardBear33@users.noreply.github.com>
2026-04-14 15:24:40 +0200

    Adapt creature_template SQL to new schema

--- data/sql/world/updates/003_update.sql
+++ data/sql/world/updates/003_update.sql
@@ -1,4 +1,4 @@
--- ===================================
+﻿-- ===================================
 --  GUILD VILLAGE: GV-Fabricator 01
 --  Idempotent inserts/updates (UPSERT)
 --  Target: acore_world
@@ -11,24 +11,8 @@ USE `acore_world`;
 -- -----------------------------------
 -- creature_template (PK: entry)
 -- -----------------------------------
-INSERT INTO `creature_template` (
-    `entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`,
-    `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`,
-    `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`,
-    `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`,
-    `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`,
-    `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`,
-    `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`,
-    `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`,
-    `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`,
-    `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`,
-    `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`,
-    `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`,
-    `flags_extra`, `ScriptName`, `VerifiedBuild`
-) VALUES
-(987448, 0, 0, 0, 0, 0, 'GV-Fabricator 01', '', NULL, 0, 85, 85, 0, 35, 1,
- 1, 1.14286, 1, 1, 20, 1, 2, 0, 13, 500, 1000, 1, 1, 1, 0, 0, 0, 0, 7, 76, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 10, 1, 1, 1, 0, 0, 1, 0, 0, 0,
- 'npc_gv_production', 12340)
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
+(987448, 0, 0, 0, 0, 0, 'GV-Fabricator 01', '', NULL, 0, 85, 85, 0, 35, 1, 1, 1.14286, 1, 1, 20, 2, 0, 13, 500, 1000, 1, 1, 1, 0, 0, 0, 0, 7, 76, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 10, 1, 1, 1, 0, 0, 1, 0, 'npc_gv_production', 12340)
 AS new
 ON DUPLICATE KEY UPDATE
   `difficulty_entry_1` = new.`difficulty_entry_1`,
@@ -50,7 +34,6 @@ ON DUPLICATE KEY UPDATE
   `speed_swim`         = new.`speed_swim`,
   `speed_flight`       = new.`speed_flight`,
   `detection_range`    = new.`detection_range`,
-  `scale`              = new.`scale`,
   `rank`               = new.`rank`,
   `dmgschool`          = new.`dmgschool`,
   `DamageModifier`     = new.`DamageModifier`,
@@ -82,8 +65,6 @@ ON DUPLICATE KEY UPDATE
   `RacialLeader`       = new.`RacialLeader`,
   `movementId`         = new.`movementId`,
   `RegenHealth`        = new.`RegenHealth`,
-  `mechanic_immune_mask`     = new.`mechanic_immune_mask`,
-  `spell_school_immune_mask` = new.`spell_school_immune_mask`,
   `flags_extra`        = new.`flags_extra`,
   `ScriptName`         = new.`ScriptName`,
   `VerifiedBuild`      = new.`VerifiedBuild`;
@@ -119,3 +100,4 @@ ON DUPLICATE KEY UPDATE
   `Name`      = new.`Name`;
 
 COMMIT;
+
--- data/sql/world/updates/006_update.sql
+++ data/sql/world/updates/006_update.sql
@@ -1,24 +1,12 @@
--- ================================
+﻿-- ================================
 -- acore_world Update: Expedition Core + Village Seller
 -- ================================
 
 -- ---- creature_template: Expedition Camp Core (987445) ----
 INSERT INTO `creature_template`
-(`entry`,`difficulty_entry_1`,`difficulty_entry_2`,`difficulty_entry_3`,
- `KillCredit1`,`KillCredit2`,`name`,`subname`,`IconName`,`gossip_menu_id`,
- `minlevel`,`maxlevel`,`exp`,`faction`,`npcflag`,
- `speed_walk`,`speed_run`,`speed_swim`,`speed_flight`,`detection_range`,
- `scale`,`rank`,`dmgschool`,`DamageModifier`,`BaseAttackTime`,`RangeAttackTime`,
- `BaseVariance`,`RangeVariance`,`unit_class`,`unit_flags`,`unit_flags2`,
- `dynamicflags`,`family`,`type`,`type_flags`,`lootid`,`pickpocketloot`,`skinloot`,
- `PetSpellDataId`,`VehicleId`,`mingold`,`maxgold`,`AIName`,`MovementType`,
- `HoverHeight`,`HealthModifier`,`ManaModifier`,`ArmorModifier`,`ExperienceModifier`,
- `RacialLeader`,`movementId`,`RegenHealth`,`mechanic_immune_mask`,
- `spell_school_immune_mask`,`flags_extra`,`ScriptName`,`VerifiedBuild`)
+(`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `flags_extra`, `ScriptName`, `VerifiedBuild`)
 VALUES
-(987445,0,0,0,0,0,'Expedition Camp Core','Upgrade Interface',NULL,0,
- 85,85,0,35,1,1,1.14286,1,1,20,1,3,0,20,500,1000,1,1,1,0,0,0,0,
- 7,76,0,0,0,0,0,0,0,'',0,1,50,1,1,1,0,0,1,0,0,0,'npc_gv_upgrades2',12340)
+(987445, 0, 0, 0, 0, 0, 'Expedition Camp Core', 'Upgrade Interface', NULL, 0, 85, 85, 0, 35, 1, 1, 1.14286, 1, 1, 20, 3, 0, 20, 500, 1000, 1, 1, 1, 0, 0, 0, 0, 7, 76, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 50, 1, 1, 1, 0, 0, 1, 0, 'npc_gv_upgrades2', 12340)
 AS new
 ON DUPLICATE KEY UPDATE
  `name`=new.`name`,
@@ -35,21 +23,9 @@ ON DUPLICATE KEY UPDATE
 
 -- ---- creature_template: Merrin the Village Broker (987454) ----
 INSERT INTO `creature_template`
-(`entry`,`difficulty_entry_1`,`difficulty_entry_2`,`difficulty_entry_3`,
- `KillCredit1`,`KillCredit2`,`name`,`subname`,`IconName`,`gossip_menu_id`,
- `minlevel`,`maxlevel`,`exp`,`faction`,`npcflag`,
- `speed_walk`,`speed_run`,`speed_swim`,`speed_flight`,`detection_range`,
- `scale`,`rank`,`dmgschool`,`DamageModifier`,`BaseAttackTime`,`RangeAttackTime`,
- `BaseVariance`,`RangeVariance`,`unit_class`,`unit_flags`,`unit_flags2`,
- `dynamicflags`,`family`,`type`,`type_flags`,`lootid`,`pickpocketloot`,`skinloot`,
- `PetSpellDataId`,`VehicleId`,`mingold`,`maxgold`,`AIName`,`MovementType`,
- `HoverHeight`,`HealthModifier`,`ManaModifier`,`ArmorModifier`,`ExperienceModifier`,
- `RacialLeader`,`movementId`,`RegenHealth`,`mechanic_immune_mask`,
- `spell_school_immune_mask`,`flags_extra`,`ScriptName`,`VerifiedBuild`)
+(`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `flags_extra`, `ScriptName`, `VerifiedBuild`)
 VALUES
-(987454,0,0,0,0,0,'Merrin the Village Broker','Village Seller',NULL,0,
- 1,1,0,35,129,1,1.14286,1,1,20,1,0,0,1,0,0,1,1,1,0,0,0,0,
- 0,0,0,0,0,0,0,0,0,'',0,1,1,1,1,1,0,0,1,0,0,0,'npc_guild_village_seller',12340)
+(987454, 0, 0, 0, 0, 0, 'Merrin the Village Broker', 'Village Seller', NULL, 0, 1, 1, 0, 35, 129, 1, 1.14286, 1, 1, 20, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 'npc_guild_village_seller', 12340)
 AS new
 ON DUPLICATE KEY UPDATE
  `name`=new.`name`,
@@ -72,3 +48,4 @@ ON DUPLICATE KEY UPDATE
  `DisplayScale`=new.`DisplayScale`,
  `Probability`=new.`Probability`,
  `VerifiedBuild`=new.`VerifiedBuild`;
+
